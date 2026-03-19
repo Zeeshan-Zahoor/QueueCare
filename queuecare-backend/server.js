1588e14a1a357bbc296690642993cb690cc8b431
Zeeshan-Zahoor <shanmohd3939@gmail.com>
2026-03-19 16:10:40 +0530

    connect server with mongoDB

--- queuecare-backend/server.js
+++ queuecare-backend/server.js
@@ -1,10 +1,20 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import { connectDB } from "./src/config/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+//Connect database firsdt
+connectDB()
+.then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}).catch((error) => {
+    console.log("Server not started! DB connection failed!: ", error.message);
+    process.exit(1);
 })
+
+
+
