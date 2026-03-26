bafe98697f567eaa2dddbb184d00552bee8e9114
Zeeshan-Zahoor <shanmohd3939@gmail.com>
2026-03-26 22:55:47 +0530

    feat: add joinQueue controller with router

--- queuecare-backend/src/controllers/clinic.controller.js
+++ queuecare-backend/src/controllers/clinic.controller.js
@@ -64,7 +64,84 @@ const getClinicDoctors = async (req, res) => {
     }
 } 
 
+const joinQueue = async (req, res) => {
+    try {
+        const { doctorId } = req.params;
+        const { name, phone, source } = req.body;
+
+        // validate
+        if(!name || !phone) {
+            return res.status(400).json({
+                message: "Name and phone is required",
+            });
+        }
+
+        // find doctor
+        const doctor = await Doctor.findById(doctorId);
+        if(!doctor) {
+            return res.status(404).json({
+                message: "Doctor not found",
+            })
+        }
+
+        //check consultation status
+        if(doctor.status !== "active") {
+            return res.status(400).json({
+                message: "Consultation is not active",
+            })
+        }
+
+        // check queue full
+        const tokensLeft = doctor.maxTokens - (doctor.currentlyServing + doctor.queue.length);
+
+        if(tokensLeft <= 0) {
+            return res.status(400).json({
+                message: "Queue full",
+            })
+        }
+        
+        // duplicate check
+        const exists = doctor.queue.some(
+            p => p.phone === phone
+        );
+
+        if(exists) {
+            return res.status(400).json({
+                message: "Patient already in queue",
+            });
+        }
+
+        //generate token
+        const newToken = doctor.lastIssuedToken + 1;
+        
+        // add patient
+        doctor.queue.push({
+            token: newToken,
+            name, 
+            phone,
+            source,
+        })
+
+        // save in database
+        await doctor.save();
+
+        // response
+        return res.status(200).json({
+            message: "Entered Queue Successfully!",
+            success: true,
+            token: newToken,
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to join queue",
+            error: error.message,
+        });
+    }
+}
 export {
     loginClinic,
     getClinicDoctors,
+    joinQueue,
 }
--- queuecare-backend/src/routes/clinic.routes.js
+++ queuecare-backend/src/routes/clinic.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginClinic, getClinicDoctors } from "../controllers/clinic.controller.js";
+import { loginClinic, getClinicDoctors, joinQueue } from "../controllers/clinic.controller.js";
 
 const router = Router();
 
 router.route("/login").post(loginClinic);
 router.route("/:clinicId/doctors").get(getClinicDoctors);
+router.route("/doctor/:doctorId/join").post(joinQueue);
 
 export default router;
